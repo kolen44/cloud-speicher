7f607259b8c6346e6e462edbebff3ab729247a50
kolen44 <malavov70@gmail.com>
2024-05-15 09:46:18 +0300

    feat: Added interceptor for uploading file . Added decorators with custom function for upload files. + Added documentation for each endpoint by swagger .

---
+++ cloud-speicher/src/files/storage.ts
@@ -0,0 +1,16 @@
+import { diskStorage } from 'multer';
+
+const generateId = () =>
+  Array(18)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16));
+
+const normalizeFilename = (req, file, callback) => {
+  const fileExtName = file.originalname.split('.').pop();
+  callback(null, `${generateId()}.${fileExtName}`);
+};
+
+export const fileStorage = diskStorage({
+  destination: './uploads',
+  filename: normalizeFilename,
+});
